dc7b739f93cdfc1b22fc45e40c3f485099265b75
youyiqin <564151947@qq.com>
2021-02-25 20:36:54 +0800

    add some content

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   "plugins": {
-    rootValue: 37.5, // vant 官方根字体 37.5
-    propList: ['*'], // 对全部属性进行转换
-    selectorBlackList: ['.norem'] // 过滤 .norem 开头的选择器,不进行rem转换    
+    "postcss-pxtorem": {
+      rootValue: 37.5, // vant 官方根字体 37.5
+      propList: ['*'], // 对全部属性进行转换
+      selectorBlackList: ['.norem'] // 过滤 .norem 开头的选择器,不进行rem转换  
+    }
   }
 }
---
+++ src/views/Home/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Home page</h1>
+    <p>we donot talk anymore</p>
+  </div>
+</template>
+
+<style lang="css">
+  h1 {
+    font-size: 20px;
+  }
+</style>
